38573bd6a21108711aae50ab8d2b251e4564c6c2
Kyle Harrison <khwebmail@gmail.com>
2014-12-04 17:17:04 +0000

    Enable building the audio HAL's

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -28,13 +28,11 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Packages
 PRODUCT_PACKAGES += \
+    audio.primary.msm7x27a \
+    audio_policy.msm7x27a \
     audio.a2dp.default \
     audio.usb.default \
     libaudioutils
-
-#PRODUCT_PACKAGES += \
-#    audio.primary.msm7x27a \
-#    audio_policy.msm7x27a
 
 PRODUCT_PACKAGES += \
     libgenlock \
