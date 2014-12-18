d1ab9ab63b7fb90d4d8eef7072782f27f39185b2
Kyle Harrison <khwebmail@gmail.com>
2014-12-18 23:27:45 +0000

    Update media codecs for L

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -87,6 +87,9 @@ PRODUCT_COPY_FILES += \
     device/huawei/msm7x27a-common/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/huawei/msm7x27a-common/configs/audio_policy.conf:system/etc/audio_policy.conf \
     device/huawei/msm7x27a-common/configs/media_codecs.xml:system/etc/media_codecs.xml \
     device/huawei/msm7x27a-common/configs/media_profiles.xml:system/etc/media_profiles.xml
