6ac6b30efddd72ebb8d0cb26db22cac5d488363c
Kyle Harrison <khwebmail@gmail.com>
2015-01-27 16:49:19 +0000

    av: Include FFMPEG codec configuration from frameworks/av

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -86,6 +86,7 @@ PRODUCT_COPY_FILES += \
     device/huawei/msm7x27a-common/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
