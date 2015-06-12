d2ef1f8beb72758c90f6376d452acd425a89b75f
fonz93 <fonz993@hotmail.it>
2015-06-13 00:43:22 +0100

    Revert "use open source lights HAL (1/2)"
    This reverts commit ab57809e758099784ffdb43f8be492edba7fdff5.
    
    With the open source lights HAL, the button backlight brightness controller it's partially broken, it allows only 2 values: 0 and 255, changing the values through /sys tuneable works fine.
    
    Revert this for now until we find a fix.

--- lights/Android.mk
+++ lights/Android.mk
@@ -1,4 +1,4 @@
-# Copyright 2015 The Android Open Source Project
+# Copyright 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,9 +27,4 @@ LOCAL_SHARED_LIBRARIES := libhardware_legacy liblog
 
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 
-# Proprietary
-LOCAL_SHARED_LIBRARIES += libhwrpc
-$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libhwrpc_intermediates/)
-$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libhwrpc_intermediates/export_includes)
-
-include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_SHARED_LIBRARY)
